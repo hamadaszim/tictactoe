b6ef1fdda88d98872e57de8002ce109fcf0fa6dd
hamadaszim <69878179+hamadaszim@users.noreply.github.com>
2020-08-20 12:46:10 +0200

    Add files via upload

--- src/main.java
+++ src/main.java
@@ -7,24 +7,24 @@ import java.util.HashMap;
 
 
 public class main {
-    static tictac game = new tictac();
-    static AI enemy = new AI();
+    static tictac game = new tictac(); // creates new game
+    static AI enemy = new AI(); // minimax algorithm
     static JFrame frame = new JFrame("Tic-tac-toe!");
     static JPanel panel = new JPanel();
-    static HashMap<Integer, JButton> buttonMap = new HashMap<>();
+    static HashMap<Integer, JButton> buttonMap = new HashMap<>(); // HashMap for buttons for later use
     static boolean WAIT = false;
-    static int LOSS = 0;
-    static int TIE = 0;
+    static int LOSS = 0; // counts users losses
+    static int TIE = 0; // counts users ties
     public static void main(String[] args) {
         frame.setVisible(true);
         panel.setVisible(true);
         panel.setLayout(new java.awt.GridLayout(3, 3));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        game.initializeBoard();
+        game.initializeBoard(); // creates a new [3][3] tictactoe board
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                final JButton button = new JButton();
-                int position = game.convertRowColToSpace(i, j);
+                final JButton button = new JButton(); //creates a button for every field in the [3][3] grid
+                int position = game.convertRowColToSpace(i, j); //converts the position to a single digit integer from [row][col] coordinates
                 button.setText("");
                 button.setBackground(Color.white);
                 button.setName(String.valueOf(position));
@@ -34,7 +34,7 @@ public class main {
                 panel.add(button);
                 button.addActionListener(
                         new ActionListener() {
-                            public void actionPerformed(ActionEvent e) {
+                            public void actionPerformed(ActionEvent e) { //listens for button clicks
                                 buttonClicked(button); }
                         });
             }
@@ -44,46 +44,43 @@ public class main {
         frame.setVisible(true);
     }
 
-    public static void buttonClicked(JButton button){
-        if(!game.isGameOVer()) {
-            boolean next = game.buttonClicked(button);
-            WAIT = false;
-            continueCheck();
-            if (next && !WAIT) {
-                game.changePlayer();
-                int position = enemy.choose(game);
-                int[] moveAI = game.convertSpaceToRowCol(position);
-                if (!game.isGameOVer()) {
-                    game.placeMark(moveAI[0], moveAI[1]);
-                    buttonMap.get(position).setText(String.valueOf(game.getCurrentPlayerMark()));
-                     continueCheck();
-                    game.changePlayer();
-                }
+    public static void buttonClicked(JButton button){ // gets called after a button is clicked
+        if(!game.isGameOVer()) { // checks whether the game is over
+            boolean next = game.buttonClicked(button); // places mark on button and returns whether it was successful
+            WAIT = false; // WAIT boolean tells the "AI" whether it should wait for user input -> used for ties.
+            continueCheck(); // checks if "AI" is needed or game is already won/tied
+            if (next && !WAIT) { // if user has placed a mark and "AI" isn't told to wait
+                game.changePlayer(); // changes current player from 'x' to '0' or vice versa
+                int position = enemy.choose(game); // stores the move the AI wants to make in a single digit Integer
+                int[] moveAI = game.convertSpaceToRowCol(position); // converting the single digit integer to [row][col] coordinates
+                game.placeMark(moveAI[0], moveAI[1]); //places the algorithms move on the board
+                buttonMap.get(position).setText(String.valueOf(game.getCurrentPlayerMark())); //updates button
+                continueCheck(); //checks if game can continue
+                game.changePlayer(); // changes current player from 'x' to '0' or vice versa
+
             }
-            continueCheck();
+            continueCheck();//checks if game can continue
         }
-        continueCheck();
     }
 
     public static void continueCheck(){
-        if(game.isGameTied()){
-            TIE++;
-            JOptionPane.showMessageDialog(null,"The game was a tie! You have tied " + TIE + " times so far!");
-            WAIT=true;
-            GameOver(0);
+        if(game.isGameTied()){ // checks for tie
+            TIE++; // increases TIE count by 1
+            WAIT=true; // tells enemy to wait
+            GameOver(0); // activates game over function indicating that it was a tie
         }
-        if(game.checkForWin()){
-            LOSS++;
-            String currentPlayer = String.valueOf(game.getCurrentPlayerMark());
-            JOptionPane.showMessageDialog(null,currentPlayer +  " Wins! You have lost " + LOSS + " times so far!");
-            GameOver(1);
+        if(game.checkForWin()){ //checks for win
+            LOSS++; // increases LOSS count by 1
+            GameOver(1); // activates game over function indicating that it was a tie
         }
     }
 
     public static void GameOver(int type){
-        game.initializeBoard();
+        JOptionPane.showMessageDialog(null, + type==1 ? (String.valueOf(game.getCurrentPlayerMark()) + " Wins! You have lost " + LOSS + " times so far!" ) : ("The game was a tie! You have tied " + TIE + " times so far!"))  ;
+        // displays the result
+        game.initializeBoard(); // initializes new board
         for(int i = 0; i < buttonMap.size(); i++){
-            buttonMap.get(i).setText("");
+            buttonMap.get(i).setText(""); // clears buttons on the GUI
         }
     }
 }
